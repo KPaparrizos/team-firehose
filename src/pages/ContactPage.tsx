4bc201ae41199700fd81166b4a1c4a3c0781cead
FallenFork <kostas.paparrizos@gmail.com>
2026-01-04 20:58:50 -0500

    thirty-third commit - expanded contact us page

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -10,17 +10,22 @@ function ContactPage() {
       <div className="contact-page-content">
         <div className="contact-page-center">
           <h1>Contact Us</h1>
-          <div className="contact-page-link">
-            <a href="mailto:teamfirehose2028@gmail.com">
-              <img src={gmailLogo} alt="Gmail logo"></img>
-            </a>
-            <h2>teamfirehose2028@gmail.com</h2>
-          </div>
-          <div className="contact-page-link">
-            <a href="https://www.instagram.com/team_firehose/">
-              <img src={instagramLogo} alt="Instagram logo"></img>
-            </a>
-            <h2>team_firehose</h2>
+          <p>
+          Feel free to reach out with questions about about our project or collaboration opportunities. Also, please follow us on Instagram for regular updates on our research, milestones, and events!
+          </p>
+          <div className="contact-page-links-wrapper">
+            <div className="contact-page-link">
+              <a href="mailto:teamfirehose2028@gmail.com">
+                <img src={gmailLogo} alt="Gmail logo"></img>
+              </a>
+              <h2>teamfirehose2028@gmail.com</h2>
+            </div>
+            <div className="contact-page-link">
+              <a href="https://www.instagram.com/team_firehose/">
+                <img src={instagramLogo} alt="Instagram logo"></img>
+              </a>
+              <h2>team_firehose</h2>
+            </div>
           </div>
         </div>
       </div>
